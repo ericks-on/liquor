cface6ccd08654a83437e2a56f4eb2b4eeffd971
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-07-16 01:03:41 +0300

    fixed qr button

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
+'use client'
+
 import Image from "next/image";
 import Header from "./ui/Header";
 import ProductCard from "./ui/ProductCard";
 import { products } from "./lib/data";
 import WaQr from "./ui/qr";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Home() {
+  const [isHiddenDivVisible, setIsHiddenDivVisible] = useState(false);
+
   return (
     <main
-      className="w-screen h-screen pt-28 flex "
+      className="w-screen h-screen pt-28 flex flex-col md:flex-row "
     >
-      <Header />
-      <div className="w-full h-auto hidden md:w-1/5 md:border-r-2 border-grey-300 md:h-full items-center p-2 md:flex flex-col gap-4">
+      <Header
+        isHiddenDivVisible={isHiddenDivVisible}
+        setIsHiddenDivVisible={setIsHiddenDivVisible}
+      />
+      <div className={clsx(
+        { "hidden md:flex": isHiddenDivVisible === false },
+        "w-full h-auto md:w-1/5 md:border-r-2 border-grey-300 md:h-full items-center p-2 flex flex-col gap-4",
+        "justify-center md:justify-start"
+      )}>
         <WaQr />
-        <p className="text-center ">To order Whatsapp 0700120294 or scan the QR code</p>
+        <p className="text-center hidden md:block">To order Whatsapp 0700120294 or scan the QR code</p>
       </div>
       <div className="flex flex-col md:w-4/5 w-full h-full md:overflow-auto">
         <p className="text-center md:hidden">To order Whatsapp 0700120294 or click red button for QR code</p>
--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
+import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa6";
 
-export default function Header() {
+interface HeaderProps {
+    isHiddenDivVisible: boolean;
+    setIsHiddenDivVisible: (visible:boolean) => void;
+}
+
+export default function Header(
+    { isHiddenDivVisible, setIsHiddenDivVisible }:HeaderProps
+) {
+    
+
+    const toggleHiddenDiv = () => {
+        setIsHiddenDivVisible(!isHiddenDivVisible);
+    };
+
     return (
         <div
             className="w-full h-28  fixed top-0 flex items-center justify-center"
@@ -11,7 +25,9 @@ export default function Header() {
                 src='/deals.jpeg'
                 width={80}
                 height={80} />
-            <div className="bg-red-600 rounded flex gap-4 items-center justify-center md:hidden absolute right-0 mr-4 cursor-pointer">
+            <div
+                onClick={toggleHiddenDiv}
+                className="bg-red-600 rounded flex gap-4 items-center justify-center md:hidden absolute right-0 mr-4 cursor-pointer">
                 <FaWhatsapp className="fill-green-700 w-10 h-10"/>
             </div>
         </div>
