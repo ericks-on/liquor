cc9db0aa75f9a6d0a713c977e0880b39697e6c9f
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-08-12 12:11:07 +0300

    updated available products

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -4,16 +4,6 @@ export const products = [
         price: 1800,
         image: '/products/trumpeter.webp',
         status: 'sealed'
-    },{
-        name: 'Tanqueray 700ml',
-        price: 2600,
-        image: '/products/tanqueray.webp',
-        status: 'sealed'
-    },{
-        name: 'Glenfiddich 12 Years 1L',
-        price: 5000,
-        image: '/products/glenfiddich-12.webp',
-        status: 'sealed'
     }, {
         name: 'Malfy Gin 1L',
         price: 3500,
@@ -24,12 +14,7 @@ export const products = [
         price: 4500,
         image: '/products/johnnie-gold-not.jpg',
         status: 'not-sealed'
-    }, {
-        name: 'Glenfiddich 12 Years 1L',
-        price: 4500,
-        image: '/products/glen-12-not.jpg',
-        status: 'not-sealed'
-    }, {
+    },, {
         name: 'Glenmorangie 10 Years',
         price: 4800,
         image: '/products/glenmorangie-not.jpg',
