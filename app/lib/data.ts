7fd678062e05397391940a3919acda5e107e146a
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-08-27 15:13:45 +0300

    product update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,46 @@
 export const products = [
     {
+        name: 'TEQUILA RES 1800 BLANCO 700mL',
+        price: 5300,
+        image: ['/products/1800-blanco.webp'],
+        status: 'sealed'
+    }, {
+        name: 'Tanqueray Rangpur lime 750ml',
+        price: 2900,
+        image: ['/products/rangpur-lime.webp'],
+        status: 'sealed'
+    }, {
+        name: 'The Botanist 1ltr',
+        price: 5000,
+        image: ['/products/The-Botanist-Gin.webp'],
+        status: 'sealed'
+    }, {
+        name: 'Ketel One Vodka 750ml',
+        price: 3500,
+        image: ['/products/ketel-one.webp'],
+        status: 'sealed'
+    }, {
+        name: 'Jose Cuervo Gold 1L',
+        price: 3200,
+        image: ['/products/jose-cuervo-gold-tequila.png'],
+        status: 'sealed'
+    }, {
+        name: 'Jose Cuervo Silver 1L',
+        price: 3200,
+        image: ['/products/jose-cuervo-silver.png'],
+        status: 'sealed'
+    }, {
+        name: 'Inverroche Verdant 750ml',
+        price: 4500,
+        image: ['/products/inverroche.png'],
+        status: 'sealed'
+    }, {
+        name: 'Gin society pink gin 750ml',
+        price: 3800,
+        image: ['/products/gin-society-pink.jpg'],
+        status: 'sealed'
+    },
+    {
         name: 'Glenfiddich 12 yrs',
         price: 5800,
         image: ['/products/glenfiddich-12.webp'],
